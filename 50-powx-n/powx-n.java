8a151365806d6829c194ccd37c2d791aed645ea4
Sumantra Das <144547836+Sumantradas01@users.noreply.github.com>
2026-08-31 19:42:30 +0530

    Time: 1 ms (9.91%) | Memory: 48.2 MB (22.16%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -2,6 +2,11 @@ class Solution {
     public double myPow(double x, int n) {
 
         long exp = n; // convert to long to handle Integer.MIN_VALUE
+        if(n==0) return 1.0;
+        if(x==0) return 0.0;
+        if(x==1) return 1.0;
+        if(x==-1 && n%2==0) return 1.0;
+        if(x==-1 && n%2 ==1) return -1.0;
 
         if (exp < 0) {
             x = 1 / x;
